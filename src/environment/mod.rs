56c125a04f92e29e82d38afdd9f35fe95e92013b
watchthelight <admin@watchthelight.org>
2025-12-24 11:52:58 -0500

    feat(environment): add EnvironmentPlugin stub

---
+++ src/environment/mod.rs
@@ -0,0 +1,16 @@
+//! Environment systems: Starfield, Nebula, Dust, Fog, Reflection
+
+use bevy::prelude::*;
+
+/// Environment plugin for cosmic backdrop
+pub struct EnvironmentPlugin;
+
+impl Plugin for EnvironmentPlugin {
+    fn build(&self, _app: &mut App) {
+        // TODO: Starfield (2000 stars)
+        // TODO: Raymarched nebula
+        // TODO: Dust particles (10,000)
+        // TODO: Volumetric fog
+        // TODO: Reflection plane
+    }
+}
