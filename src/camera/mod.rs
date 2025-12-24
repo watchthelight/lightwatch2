bb4eb2437438b95928c595aa2c169a9b80ee7a9b
watchthelight <admin@watchthelight.org>
2025-12-24 11:52:29 -0500

    feat(camera): add CameraPlugin stub

---
+++ src/camera/mod.rs
@@ -0,0 +1,16 @@
+//! Camera systems: Rig, Behaviors, DOF, Shake, Transitions
+
+use bevy::prelude::*;
+
+/// Camera plugin for cinematic camera control
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, _app: &mut App) {
+        // TODO: Camera rig
+        // TODO: Camera behaviors (drift, approach, pullback)
+        // TODO: Depth of field
+        // TODO: Trauma-based shake
+        // TODO: Cinematic transitions
+    }
+}
